37fe8efe8744ac862a2cb3041d3a69c2d51dc30a
ben-nn20 <67079393+ben-nn20@users.noreply.github.com>
2022-08-15 23:01:27 -0700

    Initial Commit

---
+++ f/fApp.swift
@@ -0,0 +1,17 @@
+//
+//  fApp.swift
+//  f
+//
+//  Created by Benjamin Nakiwala on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct fApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
